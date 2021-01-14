4a1e50dab46014fc140f5dd81d40de392ba2a523
BaileyFirman <bailey.firman@nintex.com>
2021-01-14 22:55:29 +1300

    Cleaned up HttpClient and added MarketstackService with implementations of IMarketstackService abstract members

--- MarketstackFs/Services/HttpClient.fs
+++ MarketstackFs/Services/HttpClient.fs
@@ -12,13 +12,10 @@ module HttpClient =
     let GetPageResonse<'T> url (queryString: QueryStringItem list) limit (offset: int): Async<PageResponse<'T>> =
         async {
             let pagingQueryStringItems: QueryStringItem list =
-                [
-                     "limit", limit.ToString()
-                     "offset", offset.ToString()
-                ]
+                [ "limit", limit.ToString()
+                  "offset", offset.ToString() ]
 
-            let query: QueryStringItem list =
-                queryString @ pagingQueryStringItems
+            let query: QueryStringItem list = queryString @ pagingQueryStringItems
 
             let result: PageResponse<'T> =
                 async {
@@ -35,29 +32,26 @@ module HttpClient =
     let GetAsync<'T> url apiToken (limit: int option): list<'T> =
         let pageLimit =
             match limit with
-            | Some(limit) -> limit
+            | Some (limit) -> limit
             | None -> PageResponse.MaxLimit
 
         match pageLimit > PageResponse.MaxLimit with
         | true -> raise (PageLimitException "Maximum allowed limit value is {PageResponse.MaxLimit}")
         | false ->
-            let queryString: QueryStringItem list = ["access_key", apiToken]
+            let queryString: QueryStringItem list = [ "access_key", apiToken ]
+
             let firstReponse: PageResponse<'T> =
                 GetPageResonse<'T> url queryString pageLimit PageResponse.MaxLimit
                 |> Async.RunSynchronously
 
             let pages: list<PageResponse<'T>> =
-                firstReponse.AllRequestOffsets ()
+                firstReponse.AllRequestOffsets()
                 |> List.map (fun offset -> GetPageResonse<'T> url queryString pageLimit offset)
                 |> Async.Parallel
                 |> Async.RunSynchronously
                 |> Array.toList
 
             let data =
-                pages
-                |> List.collect (fun page -> page.Data)
+                pages |> List.collect (fun page -> page.Data)
 
             data @ firstReponse.Data
-
-
-            
---
+++ MarketstackFs/Services/MarketstackService.fs
@@ -0,0 +1,61 @@
+﻿namespace MarketstackFs.Services
+
+open MarketstackFs.Interfaces.IMarketstackService
+open MarketstackFs.Entities.Exchanges.Exchange
+open MarketstackFs.Services.MarketstackOptions
+open MarketstackFs.Services.HttpClient
+open MarketstackFs.Entities.Stocks.Stock
+open MarketstackFs.Entities.Stocks.StockBar
+
+module MarketstackService =
+    type MarketstackService(options) =
+        let options: MarketstackOptions = options
+
+        let apiUrl =
+            match options.Https with
+            | true -> "https://api.marketstack.com/v1"
+            | false -> "http://api.marketstack.com/v1"
+
+
+        interface IMarketstackService with
+            member __.GetExchanges() =
+                let url = apiUrl + "/exchanges"
+                async { return GetAsync<Exchange> url options.ApiToken None }
+
+            member __.GetExchangeStocks exchangeMic =
+                let url = apiUrl + "/tickers?=" + exchangeMic
+                async { return GetAsync<Stock> url options.ApiToken None }
+
+            member __.GetStockEodBars stockSymbol fromDate toDate =
+                let dateFromStr =
+                    "&date_from=" + fromDate.ToString("yyyy-MM-dd")
+
+                let dateToStr =
+                    "&date_to=" + toDate.ToString("yyyy-MM-dd")
+
+                let url =
+                    apiUrl
+                    + "/eod?symbols="
+                    + stockSymbol
+                    + dateFromStr
+                    + dateToStr
+
+                async { return GetAsync<StockBar> url options.ApiToken None }
+
+            member __.GetStockIntraDayBars stockSymbol fromDate toDate =
+                let dateFromStr =
+                    "&date_from="
+                    + fromDate.ToString("yyyy-MM-dd HH:mm:ss")
+
+                let dateToStr =
+                    "&date_to="
+                    + toDate.ToString("yyyy-MM-dd HH:mm:ss")
+
+                let url =
+                    apiUrl
+                    + "/intraday?symbols="
+                    + stockSymbol
+                    + dateFromStr
+                    + dateToStr
+
+                async { return GetAsync<StockBar> url options.ApiToken None }
